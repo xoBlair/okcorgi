4b4aef3add9664b787e45d72277b7e05d9f352c5
mapineda Pineda <mapineda1222@gmail.com>
2015-10-15 20:24:41 -0500

    something

--- ok_corgi/models/dog.js
+++ ok_corgi/models/dog.js
@@ -2,10 +2,11 @@
 var mongoose = require ('mongoose');
 var Schema = mongoose.Schema;
 
-var dogSchema = new Schema({
+var dogSchema = new mongoose.Schema({
     name: String,
-    
 });
 
+mongoose.model('Dog', dogschema);
 
 module.exports = mongoose.model('Dog', dogSchema);
+
